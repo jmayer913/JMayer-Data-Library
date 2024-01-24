cecae18692807baf0e0bf7db25b1ee5ad3dfeb42
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2024-01-24 17:56:08 -0500

    Added equals filtering

--- JMayer.Data/Data/Query/FilterDefinition.cs
+++ JMayer.Data/Data/Query/FilterDefinition.cs
@@ -9,9 +9,19 @@ namespace JMayer.Data.Data.Query;
 public class FilterDefinition
 {
     /// <summary>
+    /// The constant for the string.Contains() method name.
+    /// </summary>
+    private const string ContainsMethodName = nameof(string.Contains);
+
+    /// <summary>
     /// The constant for the contains operator.
     /// </summary>
     public const string ContainsOperator = "contains";
+
+    /// <summary>
+    /// The constant for the equals operator.
+    /// </summary>
+    public const string EqualsOperator = "equals";
 
     /// <summary>
     /// The property gets/sets the public property/field to filter on.
@@ -29,14 +39,19 @@ public class FilterDefinition
     public string Value { get; set; } = string.Empty;
 
     /// <summary>
+    /// The constant for the object.ToString() method name.
+    /// </summary>
+    private const string ToStringMethodName = nameof(ToString);
+
+    /// <summary>
     /// The method returns a contains expression to be used for filtering.
     /// </summary>
     /// <typeparam name="T">Can be any object.</typeparam>
     /// <returns>A contains expression to be used for filtering.</returns>
     private Expression<Func<T, bool>> ToContainsExpression<T>()
     {
-        MethodInfo? stringContainsMethodInfo = typeof(string).GetMethod("Contains", [typeof(string)]);
-        MethodInfo? objectToStringMethodInfo = typeof(object).GetMethod("ToString");
+        MethodInfo stringContainsMethodInfo = typeof(string).GetMethod(ContainsMethodName, [typeof(string)]) ?? throw new MissingMethodException($"The {ContainsMethodName} method was not found.");
+        MethodInfo objectToStringMethodInfo = typeof(object).GetMethod(ToStringMethodName) ?? throw new MissingMethodException($"The {ToStringMethodName} method was not found.");
 
         var parameter = Expression.Parameter(typeof(T), "obj");
         var property = Expression.PropertyOrField(parameter, FilterOn);
@@ -45,6 +60,24 @@ public class FilterDefinition
         var contains = Expression.Call(stringConversion, stringContainsMethodInfo, value);
 
         return Expression.Lambda<Func<T, bool>>(contains, parameter);
+    }
+
+    /// <summary>
+    /// The method returns an equals expression to be used for filtering.
+    /// </summary>
+    /// <typeparam name="T">Can be any object.</typeparam>
+    /// <returns>An equals expression to be used for filtering.</returns>
+    private Expression<Func<T, bool>> ToEqualsExpression<T>()
+    {
+        MethodInfo objectToStringMethodInfo = typeof(object).GetMethod(ToStringMethodName) ?? throw new MissingMethodException($"The {ToStringMethodName} method was not found.");
+
+        var parameter = Expression.Parameter(typeof(T), "obj");
+        var property = Expression.PropertyOrField(parameter, FilterOn);
+        var stringConversion = Expression.Call(Expression.Convert(property, typeof(object)), objectToStringMethodInfo);
+        var value = Expression.Constant(Value, typeof(string));
+        var equals = Expression.Equal(stringConversion, value);
+
+        return Expression.Lambda<Func<T, bool>>(equals, parameter);
     }
 
     /// <summary>
@@ -58,6 +91,7 @@ public class FilterDefinition
         return Operator.ToLower() switch
         {
             ContainsOperator => ToContainsExpression<T>(),
+            EqualsOperator => ToEqualsExpression<T>(),
             _ => throw new NotImplementedException($"The {Operator} operator is not handled.")
         };
     }
