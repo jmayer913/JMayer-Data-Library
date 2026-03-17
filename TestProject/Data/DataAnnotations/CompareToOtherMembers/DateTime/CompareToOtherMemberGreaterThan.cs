95e8e84bea59f18c0bca46decc79f05ff226f6ae
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2026-03-17 10:08:41 -0400

    Added DateTime & DateTimeOffset unit tests

---
+++ TestProject/Data/DataAnnotations/CompareToOtherMembers/DateTime/CompareToOtherMemberGreaterThan.cs
@@ -0,0 +1,15 @@
+﻿using JMayer.Data.Data;
+using JMayer.Data.DataAnnotations;
+
+namespace TestProject.Data.DataAnnotations.CompareToOtherMembers.DateTime;
+
+/// <summary>
+/// The class is used to test the CompareToOtherMember attribute when comparing property 1 to be greater than property 2.
+/// </summary>
+internal class CompareToOtherMemberGreaterThan : DataObject
+{
+    [CompareToOtherMember(nameof(Property2), ComparisonOperation.GreaterThan)]
+    public System.DateTime Property1 { get; set; }
+
+    public System.DateTime Property2 { get; set; }
+}
---
+++ TestProject/Data/DataAnnotations/CompareToOtherMembers/DateTimeOffset/CompareToOtherMemberEqual.cs
@@ -0,0 +1,15 @@
+﻿using JMayer.Data.Data;
+using JMayer.Data.DataAnnotations;
+
+namespace TestProject.Data.DataAnnotations.CompareToOtherMembers.DateTimeOffset;
+
+/// <summary>
+/// The class is used to test the CompareToOtherMember attribute when comparing two byte properties to be equal.
+/// </summary>
+internal class CompareToOtherMemberEqual : DataObject
+{
+    [CompareToOtherMember(nameof(Property2), ComparisonOperation.Equal)]
+    public System.DateTimeOffset Property1 { get; set; }
+
+    public System.DateTimeOffset Property2 { get; set; }
+}
